cdbdfae697e01b168bc4a712948711af6bffc0e3
Musti132 <mustixd@hotmail.com>
2020-08-28 05:23:28 -0400

    First changes

---
+++ src/YourClass.php
@@ -0,0 +1,17 @@
+<?php namespace Musti\EasyMaintenance;
+
+/**
+*  A sample class
+*
+*  Use this section to define what this class is doing, the PHPDocumentator will use this
+*  to automatically generate an API documentation using this information.
+*
+*  @author yourname
+*/
+class Maintenance{
+
+   public function method1($param1){
+			return "Hello World";
+   }
+   
+}
